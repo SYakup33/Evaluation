6bfef914212b4364ad5afc834536d75f0b1ea007
unknown <yakup.senel33@gmail.com>
2022-01-03 16:26:14 +0100

    📦 NEW: initial commit, booleen

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php
     require './personnage.php';
     //Nouveau personnage avec ses stats
-    $newPerso = new Personnage("Yakup",5000,2000,1500,2000,["rabadon","cull"]);
-    // var_dump($newPerso);
+    $newPerso = new Personnage("Yakup",5000,2000,1500,2000,[]);
+    //var_dump($newPerso);
+    $newPerso->addItems("rabadon","cull");
+    $newPerso->searchItems("rabadon");
+    var_dump($newPerso);
